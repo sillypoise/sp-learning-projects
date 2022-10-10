3741ba7a2ca890e4464984944fe98bc27671783b
sillypoise <somewhatjose@gmail.com>
2022-10-10 16:08:44 -0500

    app/coffee-finder-next/card:
    Exported zod parsers for `getNearbyCoffeeStores` so we can use them in
    `getStaticProps`. Additionally also typed the Home page component with
    correct prop types using the exported inferred type from
    `getNearbyCoffeeStores` as well.

--- apps/coffee-finder-next/components/CoffeeStoreCard.tsx
+++ apps/coffee-finder-next/components/CoffeeStoreCard.tsx
@@ -1,14 +1,25 @@
 import Link from "next/link";
 
-interface CoffeeStoreCardProps {}
+interface CoffeeStoreCardProps {
+    id: string;
+    name: string;
+    address: string;
+    distance: number;
+    slug: string;
+    imgUrl: string;
+    className: string;
+}
 
-let CoffeeStoreCard: React.FC<CoffeeStoreCardProps> = () => {
+let CoffeeStoreCard: React.FC<CoffeeStoreCardProps> = ({
+    name,
+    slug,
+    address,
+}) => {
     return (
-        <Link href={`/`}>
+        <Link href={`/coffee-store/${slug}}`}>
             <article className="box">
-                <header>Store name</header>
-                <p>Store address</p>
-                <p>store URL</p>
+                <header>{name}</header>
+                <p>{address}</p>
             </article>
         </Link>
     );
