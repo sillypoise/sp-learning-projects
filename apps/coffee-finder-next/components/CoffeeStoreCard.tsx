86971e259a9bf6c22be92b4138afb01a4ba5e813
sillypoise <somewhatjose@gmail.com>
2022-10-10 16:30:48 -0500

    app/coffee-finder-next/card:
    Added `imgUrl` for card components and added it in our API schema. As of
    now its only the same image URL as none of the data that is coming have
    actual images. Also changed `next.config.js` for it to be able to fetch
    images from Unsplash

--- apps/coffee-finder-next/components/CoffeeStoreCard.tsx
+++ apps/coffee-finder-next/components/CoffeeStoreCard.tsx
@@ -1,26 +1,37 @@
+import Image from "next/image";
 import Link from "next/link";
+import { z } from "zod";
 
 interface CoffeeStoreCardProps {
-    id: string;
+    id?: string;
     name: string;
     address: string;
-    distance: number;
+    distance?: number;
     slug: string;
-    imgUrl: string;
-    className: string;
+    imgUrl?: string;
+    className?: string;
 }
 
 let CoffeeStoreCard: React.FC<CoffeeStoreCardProps> = ({
     name,
     slug,
     address,
+    imgUrl,
 }) => {
     return (
         <Link href={`/coffee-store/${slug}}`}>
-            <article className="box">
-                <header>{name}</header>
-                <p>{address}</p>
-            </article>
+            <a>
+                <article className="box">
+                    <header>{name}</header>
+                    <Image
+                        src={z.string().parse(imgUrl)}
+                        alt="default coffee store"
+                        width={640 * 0.5}
+                        height={426 * 0.5}
+                    />
+                    <p>{address}</p>
+                </article>
+            </a>
         </Link>
     );
 };
