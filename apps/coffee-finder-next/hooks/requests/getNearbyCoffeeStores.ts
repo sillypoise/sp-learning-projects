70cbb95b2af29b8d725b6496713a6b115e816f6c
sillypoise <somewhatjose@gmail.com>
2022-10-10 13:01:42 -0500

    app/coffee-finder-next/foursquareAPI
    Integrated `zod` into `getNearbyCoffeeStores` request hook for runtime
    validation of Foursquare Places API results

--- apps/coffee-finder-next/hooks/requests/getNearbyCoffeeStores.ts
+++ apps/coffee-finder-next/hooks/requests/getNearbyCoffeeStores.ts
@@ -1,3 +1,15 @@
+import { z } from "zod";
+
+let CoffeeStore = z.object({
+    name: z.string(),
+    fsq_id: z.string(),
+    link: z.string(),
+});
+
+let PlacesAPIResults = z.object({
+    results: z.array(CoffeeStore),
+});
+
 async function getNearbyCoffeeStores(latlong: string, limit: number) {
     try {
         let options: RequestInit = {
@@ -17,10 +29,13 @@ async function getNearbyCoffeeStores(latlong: string, limit: number) {
         );
 
         if (!res) throw new Error("error fetching data");
-        let data = res.json();
-        return data;
+        let data = await res.json();
+        let parsedData = PlacesAPIResults.parse(data);
+
+        return parsedData;
     } catch (error) {
         console.log(error);
+        return "oopsie daisy";
     }
 }
 
