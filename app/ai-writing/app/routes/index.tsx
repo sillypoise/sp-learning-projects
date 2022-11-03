064286c182bde012fe24adb947bedf59d2fc400d
sillypoise <somewhatjose@gmail.com>
2022-11-03 11:59:46 -0500

    app/ai-writing: turns out openai doesn't support payed access to Colombia, idk why. But it pauses the projects in its tracks. Leaving this one be for now.

--- app/ai-writing/app/routes/index.tsx
+++ app/ai-writing/app/routes/index.tsx
@@ -1,8 +1,15 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
     return (
         <main className="mlb-l">
-            <article className="center stack">
-                <h2>AI writing assistant</h2>
+            <article className="center stack [--center-width:theme(contentWidth.2)]">
+                <h1 className="text-2 max-is-max">
+                    Welcome to AI writing assistant app
+                </h1>
+                <button className="bg-scheme-dark-neutral-surface-3 p-2xs rounded-md self-center">
+                    <Link to="/writing">Get started</Link>
+                </button>
             </article>
         </main>
     );
---
+++ app/ai-writing/app/routes/writing.tsx
@@ -0,0 +1,10 @@
+export default function WritingPage() {
+    return (
+        <main className="mlb-l">
+            <article className="center stack">
+                <h3 className="text-2">Start writing your prompt!</h3>
+                <p>Hello user! Start generating your next new ideas</p>
+            </article>
+        </main>
+    );
+}
